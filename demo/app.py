20d608ae5920a16e8678bb0267523ddbc3ef216e
berlintop <bl@simmir-visiontech.com>
2024-11-25 18:30:06 +0800

    Feat : demo done

--- demo/app.py
+++ demo/app.py
@@ -10,6 +10,13 @@ from utils.utils import rotate_and_crop_rectangle
 
 
 def main():
+    lines_hand = [
+        [0, 1], [1, 2], [2, 3], [3, 4],
+        [0, 5], [5, 6], [6, 7], [7, 8],
+        [5, 9], [9, 10], [10, 11], [11, 12],
+        [9, 13], [13, 14], [14, 15], [15, 16],
+        [13, 17], [17, 18], [18, 19], [19, 20], [0, 17],
+    ]
     palm_detection = PalmDetection()
     hand_landmark = HandLandmark()
     cap = cv2.VideoCapture(0)
@@ -45,14 +52,14 @@ def main():
                 y1 = (sqn_rr_center_y + sqn_rr_size * wh_ratio / 2) * 480
                 x2 = (sqn_rr_center_x + sqn_rr_size / 2) * 640
                 y2 = (sqn_rr_center_y - sqn_rr_size * wh_ratio / 2) * 480
-                cv2.rectangle(
-                    frame,  # 参数 1: 图像
-                    (int(x1), int(y1)),  # 参数 2: 左上角点的坐标
-                    (int(x2), int(y2)),  # 参数 3: 右下角点的坐标
-                    (0, 128, 255),  # 参数 4: 矩形颜色（BGR 格式）
-                    2,  # 参数 5: 线条宽度
-                    cv2.LINE_AA  # 参数 6: 线条类型
-                )
+                # cv2.rectangle(
+                #     frame,  # 参数 1: 图像
+                #     (int(x1), int(y1)),  # 参数 2: 左上角点的坐标
+                #     (int(x2), int(y2)),  # 参数 3: 右下角点的坐标
+                #     (0, 128, 255),  # 参数 4: 矩形颜色（BGR 格式）
+                #     2,  # 参数 5: 线条宽度
+                #     cv2.LINE_AA  # 参数 6: 线条类型
+                # )
 
                 # hand: sqn_rr_size, rotation, sqn_rr_center_x, sqn_rr_center_y
                 cx = int(sqn_rr_center_x * cap_width)
@@ -83,10 +90,33 @@ def main():
                 images=cropted_rotated_hands_images,
                 rects=rects,
             )
-            print("hand_landmarks:",hand_landmarks.shape)
-            print("rotated_image_size_leftrights:", rotated_image_size_leftrights)
-            # for hand_landmark in hand_landmarks:
-            #     print(hand_landmark)
+            # print("hand_landmarks.shape:",hand_landmarks.shape)
+            # print("hand_landmarks.len:", len(hand_landmarks))
+            # print("hand_landmarks:", hand_landmarks)
+            # print("rotated_image_size_leftrights:", rotated_image_size_leftrights)
+            for landmark in hand_landmarks:
+                # print("landmark:",landmark)
+                lines = np.asarray(
+                    [
+                        np.array([landmark[point] for point in line]).astype(np.int32) for line in lines_hand
+                    ]
+                )
+                cv2.polylines(
+                    frame,
+                    lines,
+                    False,
+                    (255, 0, 0),
+                    3,
+                    cv2.LINE_AA,
+                )
+                for point in landmark:
+                    x, y = point
+                    cv2.circle(
+                        frame,
+                        (x, y),
+                        2,
+                        (0, 128, 255),
+                        -1)
 
         image = draw_info(frame, fps)
         cv2.imshow('摄像头画面', image)
